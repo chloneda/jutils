44276845a1977f26e1db94cd51c6c81ee68624af
chl <chenhailin_vip@163.com>
2019-10-10 19:41:51 +0800

    update es

--- src/test/java/com/chloneda/jutils/elasticsearch/ElasticsearchUtilsTest.java
+++ src/test/java/com/chloneda/jutils/elasticsearch/ElasticsearchUtilsTest.java
@@ -1,10 +1,12 @@
 package com.chloneda.jutils.elasticsearch;
 
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.metadata.MetaData;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.net.UnknownHostException;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -21,26 +23,73 @@ public class ElasticsearchUtilsTest {
         esConfig.setPort(9300);
         esConfig.setSniff(false);
         esConfig.setClusterName("elastic");
-        client = ElasticsearchUtils.createClient(esConfig);
+        client = ElasticsearchUtils.initClient(esConfig);
     }
 
     @Test
-    public void testGetAllIndices() {
-        Set set = ElasticsearchUtils.getAllIndices();
+    public void testGetAllIndexes() {
+        Set set = ElasticsearchUtils.getAllIndexes();
         System.out.println("All index:{} " + set);
     }
 
     @Test
-    public void testIsExitIndice() {
-        boolean exit = ElasticsearchUtils.isExitIndice("chl_test");
+    public void testIsExitIndex() {
+        boolean exit = ElasticsearchUtils.isExitIndex("chl_test");
         System.out.println("The index is exit:{} " + exit);
     }
 
     @Test
-    public void testIsExistsIndiceType() {
-        boolean exit = ElasticsearchUtils.isExistsIndiceType("chl_test", "person");
+    public void testIsExistsIndexType() {
+        boolean exit = ElasticsearchUtils.isExistsIndexType("chl_test", "person");
         System.out.println("The index type is exit:{} " + exit);
     }
 
+    @Test
+    public void testGetMetaData() {
+        MetaData metaData = ElasticsearchUtils.getMetaData();
+        System.out.println("The ES metaData is :{} " + metaData.clusterUUID());
+    }
+
+    @Test
+    public void testCreateIndex() {
+        boolean isCreated = ElasticsearchUtils.createIndex("http_log");
+        System.out.println("The ES index is created:{} " + isCreated);
+    }
+
+    @Test
+    public void testCreateType() {
+        boolean isCreateType = ElasticsearchUtils.createType("http_log", "log");
+        System.out.println("The ES index type is created:{} " + isCreateType);
+    }
+
+    @Test
+    public void testGetIndexAllTypes() {
+        Set typeSet = ElasticsearchUtils.getIndexAllTypes("http_log");
+        System.out.println("The ES index all type is :{} " + typeSet);
+    }
+
+    @Test
+    public void testDeleteIndex() {
+        boolean isDeleted = ElasticsearchUtils.deleteIndex("http_log");
+        System.out.println("The ES index is deleted:{} " + isDeleted);
+    }
+
+    @Test
+    public void testDeleteIndexWithType() {//?
+        boolean isDeleted = ElasticsearchUtils.deleteIndexWithType("http_log","log");
+        System.out.println("The ES index with type is deleted:{} " + isDeleted);
+    }
+
+    @Test
+    public void testGetFieldsIndexTypes() {
+        Map map = ElasticsearchUtils.getFieldsIndexTypes("172.21.8.28",9200,"chl_test","person");
+        System.out.println("The ES index fields is :{} " + map);
+    }
+
+    @Test
+    public void testIsConnected() {
+        boolean isConnected = ElasticsearchUtils.isConnected();
+        System.out.println("The ES is connected:{} " + isConnected);
+    }
 
 }
