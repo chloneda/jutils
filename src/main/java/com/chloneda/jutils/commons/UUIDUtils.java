a2f465e99132c613024da8c3e1362b35f4ed4bef
allen <chloneda@gmail.com>
2019-11-04 09:15:30 +0800

    Serialization

---
+++ src/main/java/com/chloneda/jutils/commons/UUIDUtils.java
@@ -0,0 +1,75 @@
+package com.chloneda.jutils.commons;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.UUID;
+
+/**
+ * @author chloneda
+ * @description:
+ */
+public abstract class UUIDUtils {
+    public UUIDUtils() {
+    }
+
+    public static String UUID() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+
+    public static String compressedUUID() {
+        UUID uuid = UUID.randomUUID();
+        return compressedUUID(uuid);
+    }
+
+    public static String compactUUID() {
+        UUID uuid = UUID.randomUUID();
+        String result = uuid.toString();
+        return result.replaceAll("-", "");
+    }
+
+    protected static String compressedUUID(UUID uuid) {
+        byte[] byUUID = new byte[16];
+        long least = uuid.getLeastSignificantBits();
+        long most = uuid.getMostSignificantBits();
+        long2bytes(most, byUUID, 0);
+        long2bytes(least, byUUID, 8);
+        String compressUUID = Base64.encodeBase64URLSafeString(byUUID);
+        return compressUUID;
+    }
+
+    protected static void long2bytes(long value, byte[] bytes, int offset) {
+        for (int i = 7; i > -1; --i) {
+            bytes[offset++] = (byte) ((int) (value >> 8 * i & 255L));
+        }
+
+    }
+
+    public static String compress(String uuidString) {
+        UUID uuid = UUID.fromString(uuidString);
+        return compressedUUID(uuid);
+    }
+
+    public static String uncompress(String compressedUUID) {
+        if (compressedUUID.length() != 22) {
+            throw new IllegalArgumentException("Invalid uuid!");
+        } else {
+            byte[] byUUID = Base64.decodeBase64(compressedUUID + "==");
+            long most = bytes2long(byUUID, 0);
+            long least = bytes2long(byUUID, 8);
+            UUID uuid = new UUID(most, least);
+            return uuid.toString();
+        }
+    }
+
+    protected static long bytes2long(byte[] bytes, int offset) {
+        long value = 0L;
+
+        for (int i = 7; i > -1; --i) {
+            value |= ((long) bytes[offset++] & 255L) << 8 * i;
+        }
+
+        return value;
+    }
+}
+
